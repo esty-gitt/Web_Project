66a7466520951c77801804e776007f182c3b1fd8
esty-gitt <esty5664@gmail.com>
2025-05-10 22:32:19 +0300

    Creating and starting

--- server/config/conndb.js
+++ server/config/conndb.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose")
 const connectDB=async()=>{
     try
     {
-        console.log("ff")
+      
         await mongoose.connect(process.env.uriDB)
     }
     catch(error)
